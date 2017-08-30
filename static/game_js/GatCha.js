46f96af1af4933b40c2412e6fe1abf881f2551d0
Ravi <accounts1237@naver.com>
2017-08-30 19:41:17 +0900

    충돌 구현 완료, 연출 구현

--- static/game_js/GatCha.js
+++ static/game_js/GatCha.js
@@ -8,9 +8,15 @@ var container = new PIXI.Container();
 
 document.body.appendChild(renderer.view);
 
-var ball;
-var ballBody;
+var ball = new Array();
+var ballBody = new Array();
+
 var backGround;
+var backGround2;
+var backGround3;
+
+var ballMaxCount;
+
 var exit;
 // matter Init and Link Object
 function Init() {
@@ -22,17 +28,95 @@ function Init() {
     var engine = Engine.create();
     var runner = Runner.create();
 
-    ballBody = Bodies.circle(380, 100, 40, 10);
-    var ground = Bodies.rectangle(400, 1050, 810, 60, {isStatic: true});
+    backGround2 = Bodies.polygon(200, 460, 3, 60);
 
-    World.add(engine.world, [ballBody, ground]);
+    var ground = Bodies.rectangle(width / 2, 1060, 80, 20, {isStatic: true});
+
+    backGround2 = Bodies.rectangle(150, 844, 524, 20, { isStatic: true, angle: Math.PI * 0.26 });
+    backGround3 = Bodies.rectangle((width) - 150, 844, 524, 20, { isStatic: true, angle: -Math.PI * 0.26 });
 
     Engine.run(engine);
     Runner.run(runner, engine);
 
-    backGround = pah(container, "./static/Img/backGround.png", 0, 0, 750, 1344);
-    ball = pah(container, "./static/Img/ball.png", ballBody.x, ballBody.y, ballBody.xOffset, ballBody.yOffset);
-    exit = pah(container, "./static/Img/Exit.png", 287, 1000, 177, 167);
+    backGround = pah(container, "./static/Img/BackGround.png", 0, 0, 750, 1344);
+
+    var xCount = 60;
+    var yCount = 0;
+
+    ballMaxCount = 50;
+
+    for (var i = 0; i < ballMaxCount ; i++) {
+        if (xCount > width) {
+            yCount += 80;
+            xCount = 0;
+        }
+
+        if (i % 5 == 0) {
+            ballBody[i] = Bodies.circle((xCount), (yCount), 40);
+            ball[i] = pah(container, "./static/Img/BigBall.png", ballBody[i].x, ballBody[i].y, 80, 80, {anchor : 0.5});
+            World.add(engine.world, ballBody[i]);
+
+            xCount += 80;
+        }
+        else
+        {
+            ballBody[i] = Bodies.circle((xCount), (yCount), 30);
+            ball[i] = pah(container, "./static/Img/ball.png", ballBody[i].x, ballBody[i].y, 60, 60, {anchor: 0.5});
+            World.add(engine.world, ballBody[i]);
+
+            xCount += 80;
+        }
+
+    }
+
+    /*for (var i = 0; i < ballMaxCount - 10; i++)
+    {
+        if (xCount > width) {
+            yCount += 60;
+            xCount = 0;
+        }
+
+        ballBody[i] = Bodies.circle((xCount), (yCount), 30);
+        ball[i] = pah(container, "./static/Img/ball.png", ballBody[i].x, ballBody[i].y, 60, 60, {anchor:  0.5});
+        World.add(engine.world, ballBody[i]);
+
+        xCount += 60;
+    }
+
+    for (var i = ballMaxCount - 10; i < ballMaxCount; i++)
+    {
+        if (xCount > width) {
+            yCount += 80;
+            xCount = 0;
+        }
+
+        ballBody[i] = Bodies.circle((xCount), (yCount), 40);
+        ball[i] = pah(container, "./static/Img/BigBall.png", ballBody[i].x, ballBody[i].y, 80, 80, {anchor : 0.5});
+        World.add(engine.world, ballBody[i]);
+
+        xCount += 80;
+    }*/
+
+    exit = pah(container, "./static/Img/Exit.png", 287, 1010, 177, 167);
+
+    World.add(engine.world, [ground, backGround2, backGround3]);
+
+    // create renderer
+    var render = Matter.Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+
+            width: width,
+            height: height,
+            showCollisions: true,
+            showPositions: true,
+            showBounds: true,
+            showIds: true,
+        }
+    });
+
+    Matter.Render.run(render);
 }
 
 Init();
@@ -42,8 +126,8 @@ function Loop() {
     requestAnimationFrame(Loop);
     renderer.render(container);
 
-    console.log(ballBody.position.x);
-
-    ball.position.x = ballBody.position.x;
-    ball.position.y = ballBody.position.y;
+    for (var i = 0; i < ballMaxCount; i++) {
+        ball[i].position.x = ballBody[i].position.x;
+        ball[i].position.y = ballBody[i].position.y;
+    }
 }
